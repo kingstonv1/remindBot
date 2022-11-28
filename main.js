e6feb31ca8ce6710406f39397ba039a8e232faa9
kingstonv1 <kingstongise@gmail.com>
2022-11-28 14:09:42 -0500

    remove google cloud stuff

--- main.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-console.log('start');
-async function ummm() 
-{
-    const res = await axios.get('https://api.sampleapis.com/cartoons/cartoons2D');
-    console.log(res.status);
-}
